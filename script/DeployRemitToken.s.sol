2e4d76288c66b595f0895e80189c9acbf8ebd27c
Mellow <mainamachariake@example.com>
2026-04-09 17:40:41 +0300

    style: apply forge fmt formatting

--- script/DeployRemitToken.s.sol
+++ script/DeployRemitToken.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.33;
 
 import {Script, console} from "forge-std/Script.sol";
 import {RemitToken} from "../src/RemitToken.sol";
